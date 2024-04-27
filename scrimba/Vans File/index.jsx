a7075bd13b8be4260bbd100d230874fe86b51fa2
Benjamin Schoolland <bschoolland@gmail.com>
2024-04-27 14:44:01 -0700

    make vans file

---
+++ scrimba/Vans File/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans"
+
+import "./server"
+
+/**
+ Created vans component
+ Updated navbar, and added route to vans
+ */
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link> 
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
---
+++ scrimba/Vans File/pages/Vans.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function Vans() {
+    return (
+        <div className="vans-container">
+            <h1>Vans go here!</h1>
+        </div>
+    )
+};
